4a663ef295536f3821f13bddfd77c113638c91d8
Bernardo Viggiano <beviggiano@gmail.com>
2024-05-07 09:01:51 -0300

    feat(controleEstoque): listagem de itens do inventário

--- controller/controleEstoque.ts
+++ controller/controleEstoque.ts
@@ -5,7 +5,7 @@ import prompt from 'prompt-sync';
 const service = new EstoqueService();
 const input = prompt();
 
-function displayMenu(){
+export function displayMenu(){
     console.log("\nEscolha uma das seguintes opções:");
     console.log("1: Adicionar item ao estoque");
     console.log("2: Remover item do estoque");
@@ -19,7 +19,7 @@ function displayMenu(){
     console.log("0: Sair");
 }
 
-async function addItem() {
+export async function addItem() {
     const name = input('Digite o nome do item: ');
     const weight = parseFloat(input('Digite o peso do item (kg): '));
     const value = parseFloat(input('Digite o valor do item (R$): '));
@@ -29,8 +29,14 @@ async function addItem() {
     console.log("Item adicionado com sucesso.");
 }
 
-async function removeItem() {
+export async function removeItem() {
     const index = parseInt(input('Digite o índice do item a ser removido: '), 10);
     await service.removeItem(index);
     console.log("Item removido com sucesso!");
+}
+
+export async function listItems(){
+    const items = await service.listItems();
+    console.log("Itens no estoque: ");
+    items.forEach((item, index) => console.log(`${index}: ${item.name}, Peso: ${item.weight} kg, Valor: ${item.value} R$, Quantidade: ${item.quantity}`));
 }
